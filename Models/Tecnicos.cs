18d6acf5a05e3bb629d1dd0c973b5a424cde00cf
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2024-05-09 14:39:15 -0400

    Agregando la clase.

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDEeTecnico.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El sueldo es requerido")]
+        public float SueldoHora { get; set; }
+    }
+}
