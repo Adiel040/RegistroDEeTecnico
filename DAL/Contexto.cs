53a1db303223f41aedc3bb75e3a5d72af1b52d60
Adiel040 <133602314+Adiel040@users.noreply.github.com>
2024-05-09 14:51:25 -0400

    Agrengando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDEeTecnico.Models;
+
+namespace RegistroDEeTecnico.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> Options)
+        : base(Options) { }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+    }
+}
